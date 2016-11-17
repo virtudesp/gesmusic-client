8762ef4f444d6c15eaeea9032014a871b58b8fe0
rafa <rafa@rafa-K55A>
2016-11-17 12:43:27 +0100

    foreigns

--- public_html/js/tipodocumento/viewpop.js
+++ public_html/js/tipodocumento/viewpop.js
@@ -27,22 +27,30 @@
  */
 'use strict';
 
-moduloDocumento.controller('DocumentoViewController', ['$scope', '$routeParams', 'serverService', '$location',
-    function ($scope, $routeParams, serverService, $location) {
-        $scope.title = "Vista de documento";
+
+moduloUsuario.controller('TipodocumentoViewpopController', ['$scope', '$routeParams', 'serverService', '$location', '$uibModalInstance', 'id',
+    function ($scope, $routeParams, serverService, $location, $uibModalInstance, id) {
+        $scope.status = null;
+        $scope.title = "Vista de tipo de documento";
         $scope.icon = "fa-file-text-o";
-        $scope.ob = 'documento';
-        $scope.id = $routeParams.id;
+        $scope.ob = 'tipodocumento';
+        $scope.id = id;
         serverService.promise_getOne($scope.ob, $scope.id).then(function (response) {
-            $scope.bean = response.data.message;
+            if (response.status == 200) {
+                if (response.data.status == 200) {
+                    $scope.status = null;
+                    $scope.bean = response.data.message;
+                } else {
+                    $scope.status = "Error en la recepción de datos del servidor";
+                }
+            } else {
+                $scope.status = "Error en la recepción de datos del servidor";
+            }
+        }).catch(function (data) {
+            $scope.status = "Error en la recepción de datos del servidor";
         });
-        $scope.close = function () {
-            $location.path('/home');
-        };
-        $scope.plist = function () {
-            $location.path('/documento/plist');
-        };
-        $scope.back = function () {
-            window.history.back();
-        };
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        }
+
     }]);
