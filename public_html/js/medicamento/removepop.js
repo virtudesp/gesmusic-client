4797a1d9a08630cb20497c220b6bfc61b5947864
raznara <raznara@S07-PROFE>
2016-12-13 16:14:32 +0100

    virtudes - medicamento

--- public_html/js/medicamento/removepop.js
+++ public_html/js/medicamento/removepop.js
@@ -1,21 +1,21 @@
-/*
+/* 
  * Copyright (c) 2015 by Rafael Angel Aznar Aparici (rafaaznar at gmail dot com)
- *
- * sisane: The stunning micro-library that helps you to develop easily
+ * 
+ * sisane: The stunning micro-library that helps you to develop easily 
  *             AJAX web applications by using Angular.js 1.x & sisane-server
  * sisane is distributed under the MIT License (MIT)
  * Sources at https://github.com/rafaelaznar/
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,45 +23,27 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *
+ * 
  */
-
 'use strict';
-moduloMedicamento.controller('MedicamentoEditController', ['$scope', '$routeParams', '$location', 'medicamentoService', 'serverService', 'postService', 'sharedSpaceService', '$filter', '$uibModal',
-    function ($scope, $routeParams, $location, medicamentoService, serverService, postService, sharedSpaceService, $filter, $uibModal) {
+
+
+moduloMedicamento.controller('MedicamentoRemovepopController', ['$scope', '$routeParams', 'serverService', 'medicamentoService', '$location', '$uibModalInstance', 'id',
+    function ($scope, $routeParams, serverService, medicamentoService, $location, $uibModalInstance, id) {
         $scope.fields = medicamentoService.getFields();
         $scope.obtitle = medicamentoService.getObTitle();
         $scope.icon = medicamentoService.getIcon();
         $scope.ob = medicamentoService.getTitle();
-        $scope.title = "Editando un " + $scope.obtitle;
-        $scope.op = "plist";
+        $scope.title = "Borrado de " + $scope.obtitle;
+        $scope.id = id;
         $scope.status = null;
-        $scope.error = true;
         $scope.debugging = serverService.debugging();
-        $scope.bean = {};
-        $scope.id = $routeParams.id;
-        serverService.promise_getOne($scope.ob, $scope.id).then(function (response) {
-            if (response.status == 200) {
-                if (response.data.status == 200) {
-                    $scope.status = null;
-                    $scope.bean = response.data.message;
-                } else {
-                    $scope.status = "Error en la recepción de datos del servidor";
-                }
-            } else {
-                $scope.status = "Error en la recepción de datos del servidor";
-            }
-        }).catch(function (data) {
-            $scope.status = "Error en la recepción de datos del servidor";
-        });
-        $scope.save = function () {
-            var jsonToSend = {json: JSON.stringify(serverService.array_identificarArray($scope.bean))};
-            serverService.promise_setOne($scope.ob, jsonToSend).then(function (response) {
+        function getData() {
+            serverService.promise_getOne($scope.ob, $scope.id).then(function (response) {
                 if (response.status == 200) {
                     if (response.data.status == 200) {
-                        $scope.response = response;
-                        $scope.status = "El registro " + $scope.obtitle + " se ha modificado ... id = " + $scope.bean.id;
-                        $scope.bean.id = $scope.bean.id;
+                        $scope.status = null;
+                        $scope.bean = response.data.message;
                     } else {
                         $scope.status = "Error en la recepción de datos del servidor";
                     }
@@ -71,24 +53,31 @@ moduloMedicamento.controller('MedicamentoEditController', ['$scope', '$routePara
             }).catch(function (data) {
                 $scope.status = "Error en la recepción de datos del servidor";
             });
-            ;
-        };
-        $scope.back = function () {
-            window.history.back();
-        };
-        $scope.close = function () {
-            $location.path('/home');
-        };
-        $scope.plist = function () {
-            $location.path('/' + $scope.ob + '/plist');
-        };
-        $scope.chooseOne = function (nameForeign, foreignObjectName, contollerName) {
-            var modalInstance = $uibModal.open({
-                templateUrl: 'js/' + foreignObjectName + '/selection.html',
-                controller: contollerName,
-                size: 'lg'
-            }).result.then(function (modalResult) {
-                $scope.bean[nameForeign].id = modalResult;
+        }
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        }
+        $scope.remove = function () {
+            serverService.promise_removeOne($scope.ob, $scope.id).then(function (response) {
+                if (response.status == 200) {
+                    if (response.data.status == 200) {
+                        if (response.data.message == 1) {
+                            $scope.status = "El registro " + $scope.obtitle + " se ha eliminado.";
+                            $uibModalInstance.close(true);
+                            getData();
+                            return false;
+                        } else {
+                            $scope.status = "Error en el borrado de datos del servidor";
+                        }
+                    } else {
+                        $scope.status = "Error en la recepción de datos del servidor";
+                    }
+                } else {
+                    $scope.status = "Error en la recepción de datos del servidor";
+                }
+            }).catch(function (data) {
+                $scope.status = "Error en la recepción de datos del servidor";
             });
-        };       
+        }
+        getData();
     }]);
--- public_html/js/medicamento/view.js
+++ public_html/js/medicamento/view.js
@@ -1,21 +1,21 @@
-/*
+/* 
  * Copyright (c) 2015 by Rafael Angel Aznar Aparici (rafaaznar at gmail dot com)
- *
- * sisane: The stunning micro-library that helps you to develop easily
+ * 
+ * sisane: The stunning micro-library that helps you to develop easily 
  *             AJAX web applications by using Angular.js 1.x & sisane-server
  * sisane is distributed under the MIT License (MIT)
  * Sources at https://github.com/rafaelaznar/
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,23 +23,21 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *
+ * 
  */
 
 'use strict';
-moduloMedicamento.controller('MedicamentoEditController', ['$scope', '$routeParams', '$location', 'medicamentoService', 'serverService', 'postService', 'sharedSpaceService', '$filter', '$uibModal',
-    function ($scope, $routeParams, $location, medicamentoService, serverService, postService, sharedSpaceService, $filter, $uibModal) {
+
+moduloMedicamento.controller('MedicamentoViewController', ['$scope', '$routeParams', 'serverService', 'medicamentoService', '$location',
+    function ($scope, $routeParams, serverService, medicamentoService, $location) {
         $scope.fields = medicamentoService.getFields();
         $scope.obtitle = medicamentoService.getObTitle();
         $scope.icon = medicamentoService.getIcon();
         $scope.ob = medicamentoService.getTitle();
-        $scope.title = "Editando un " + $scope.obtitle;
-        $scope.op = "plist";
-        $scope.status = null;
-        $scope.error = true;
-        $scope.debugging = serverService.debugging();
-        $scope.bean = {};
+        $scope.title = "Vista de " + $scope.obtitle;
         $scope.id = $routeParams.id;
+        $scope.status = null;
+        $scope.debugging=serverService.debugging();
         serverService.promise_getOne($scope.ob, $scope.id).then(function (response) {
             if (response.status == 200) {
                 if (response.data.status == 200) {
@@ -54,41 +52,13 @@ moduloMedicamento.controller('MedicamentoEditController', ['$scope', '$routePara
         }).catch(function (data) {
             $scope.status = "Error en la recepción de datos del servidor";
         });
-        $scope.save = function () {
-            var jsonToSend = {json: JSON.stringify(serverService.array_identificarArray($scope.bean))};
-            serverService.promise_setOne($scope.ob, jsonToSend).then(function (response) {
-                if (response.status == 200) {
-                    if (response.data.status == 200) {
-                        $scope.response = response;
-                        $scope.status = "El registro " + $scope.obtitle + " se ha modificado ... id = " + $scope.bean.id;
-                        $scope.bean.id = $scope.bean.id;
-                    } else {
-                        $scope.status = "Error en la recepción de datos del servidor";
-                    }
-                } else {
-                    $scope.status = "Error en la recepción de datos del servidor";
-                }
-            }).catch(function (data) {
-                $scope.status = "Error en la recepción de datos del servidor";
-            });
-            ;
-        };
-        $scope.back = function () {
-            window.history.back();
-        };
         $scope.close = function () {
             $location.path('/home');
         };
         $scope.plist = function () {
             $location.path('/' + $scope.ob + '/plist');
         };
-        $scope.chooseOne = function (nameForeign, foreignObjectName, contollerName) {
-            var modalInstance = $uibModal.open({
-                templateUrl: 'js/' + foreignObjectName + '/selection.html',
-                controller: contollerName,
-                size: 'lg'
-            }).result.then(function (modalResult) {
-                $scope.bean[nameForeign].id = modalResult;
-            });
-        };       
+        $scope.back = function () {
+            window.history.back();
+        };
     }]);
