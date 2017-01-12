0b78d7fb717cc3d29a4a8e931022e4ef7953609b
rafa aznar <rafaaznar@gmail.com>
2017-01-12 13:37:15 +0100

    cpaginationspa component (used in mediacamento)

---
+++ public_html/js/system/components/plistspa/cpagination.js
@@ -0,0 +1,28 @@
+'use strict';
+moduloDirectivas.component('cplistpaginationspa', {
+    restrict: 'E',
+    bindings: {
+        ob: '<',
+        op: '<',
+        numpage: '<',
+        rpp: '<',
+        pages: '<',
+        neighbourhood: '<',
+        filterParams: '<',
+        orderParams: '<',
+        sfilterParams: '<',
+        pageSelectionEvent: '&'
+    },
+    templateUrl: 'js/system/components/plistspa/cpagination.html',
+    controllerAs: 'pf',
+    controller: ['serverService', function (serverService) {
+            var self = this;
+            self.gotopage = function (numpage) {
+                self.pageSelectionEvent({'selectedPageNumber': numpage})
+                return false;
+            };
+            self.getRangeArray = serverService.getRangeArray;
+            self.evaluateMin = serverService.evaluateMin;
+            self.evaluateMax = serverService.evaluateMax;
+        }]
+});
