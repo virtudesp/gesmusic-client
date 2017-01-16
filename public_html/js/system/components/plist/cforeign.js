bf633ba65800f4e54efcff3ad286346fac2a84a9
rafa aznar <rafaaznar@gmail.com>
2017-01-16 14:20:08 +0100

    cforeign component for plist applied in analitica

---
+++ public_html/js/system/components/plist/cforeign.js
@@ -0,0 +1,18 @@
+'use strict';
+moduloDirectivas.component('cforeign', {
+    restrict: 'E',
+    bindings: {
+        obf: '<',
+        opf: '<',
+        idf: '<',
+        descf: '<'      
+    },
+    templateUrl: 'js/system/components/plist/cforeign.html',
+    controllerAs: 'cf',
+    controller: ['serverService', '$location', function (serverService, $location) {
+            var self = this;
+           
+        }]
+});
+
+
