da7d39a51015e687b96d0962102df304045d9066
manuel-fp <manuel-fp@hotmail.com>
2017-01-19 20:57:58 +0100

    Componente id Manuel Florido (#42)
    * Componente id Manuel Florido
    
    * Componente id Manuel Florido
    
    * Componente id Manuel Florido

---
+++ public_html/js/system/components/key/key.js
@@ -0,0 +1,15 @@
+moduloDirectivas.component('key', {
+    templateUrl: "js/system/components/key/key.html",
+    controllerAs: 'k',
+    controller: key,
+    bindings: {
+        ide: '=',
+        name: '<',
+        disable: '<'
+    }
+
+});
+
+function key() {
+
+}
