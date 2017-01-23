26e29e5d5f74c3e0e7c4c54f6c21d5199232dcee
nesbarse <barrenosegarra2@gmail.com>
2017-01-23 18:48:55 +0100

    created link from one paciente to his/her episodios (#46)

--- public_html/js/episodio/plist.js
+++ public_html/js/episodio/plist.js
@@ -51,6 +51,11 @@ moduloEpisodio.controller('EpisodioPListController', ['$scope', '$routeParams', 
         $scope.status = null;
         $scope.debugging = serverService.debugging();
         function getDataFromServer() {
+            
+            if($routeParams.id){
+                $scope.filterExpression = "and,id_paciente,equa," + $routeParams.id
+            }
+            
             serverService.promise_getCount($scope.ob, $scope.filterExpression).then(function (response) {
                 if (response.status == 200) {
                     $scope.registers = response.data.message;
