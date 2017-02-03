b445e24a7e5d0a289398e6f3fdc730a1a418db22
raznara <raznara@S07-PROFE>
2017-02-03 20:06:42 +0100

    fix order & filter in episodio plist

--- public_html/js/episodio/plist.js
+++ public_html/js/episodio/plist.js
@@ -52,6 +52,7 @@ moduloEpisodio.controller('EpisodioPListController', ['$scope', '$routeParams', 
         $scope.filterExpression = serverService.getFilterExpression($routeParams.filter, $routeParams.sfilter);
         $scope.status = null;
         $scope.debugging = serverService.debugging();
+        $scope.url = $scope.ob + '/' + $scope.op;
         function getDataFromServer() {          
             serverService.promise_getCount($scope.ob, $scope.filterExpression).then(function (response) {
                 if (response.status == 200) {
