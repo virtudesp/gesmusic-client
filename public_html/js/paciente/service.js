6237a5af01738906bc4201736def0950b717d56a
raznara <raznara@S07-PROFE>
2017-02-06 18:17:26 +0100

    analitica & episodio plist foreigns

--- public_html/js/paciente/service.js
+++ public_html/js/paciente/service.js
@@ -10,16 +10,16 @@ moduloServicios.factory('pacienteService', ['serverService', function (serverSer
                     {name: "segundo_apellido", shortname: "Seg.apellido", longname: "Segundo apellido", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("nombre"), help: serverService.getRegExpl("nombre")},
                     {name: "login", shortname: "Login", longname: "Login", visible: false, type: "text", required: true, maxlength: 255, pattern: ""},
                     {name: "password", shortname: "Pass", longname: "Password", visible: false, type: "text", required: true, maxlength: 255, pattern: ""},
-                    {name: "estado", shortname: "Estado", longname: "Estado", visible: true, type: "text", required: true, maxlength: 255, pattern: ""},
+                    {name: "estado", shortname: "Estado", longname: "Estado", visible: false, type: "text", required: true, maxlength: 255, pattern: ""},
                     {name: "direccion", shortname: "Direcc.", longname: "Direccion", visible: false, type: "text", required: true, maxlength: 255, pattern: ""},
-                    {name: "ciudad", shortname: "Ciudad", longname: "Ciudad", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("nombre"), help: serverService.getRegExpl("nombre")},
-                    {name: "codigopostal", shortname: "Cod. Postal", longname: "Codigo Postal", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("codigopostal"), help: serverService.getRegExpl("nombre")},
-                    {name: "provincia", shortname: "Prov.", longname: "Provincia", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("nombre"), help: serverService.getRegExpl("nombre")},
+                    {name: "ciudad", shortname: "Ciudad", longname: "Ciudad", visible: false, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("nombre"), help: serverService.getRegExpl("nombre")},
+                    {name: "codigopostal", shortname: "Cod. Postal", longname: "Codigo Postal", visible: false, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("codigopostal"), help: serverService.getRegExpl("nombre")},
+                    {name: "provincia", shortname: "Prov.", longname: "Provincia", visible: false, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("nombre"), help: serverService.getRegExpl("nombre")},
                     {name: "pais", shortname: "Pais", longname: "Pais", visible: false, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("nombre"), help: serverService.getRegExpl("nombre")},
                     {name: "email", shortname: "Email", longname: "Email", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("email"), help: serverService.getRegExpl("email")},
                     {name: "telefono", shortname: "Tel.", longname: "Telefono", visible: true, type: "text", required: true, maxlength: 255, pattern: serverService.getRegExpr("telefono"), help: serverService.getRegExpl("telefono")},
-                    {name: "obj_servicio", shortname: "Tipo serv.", longname: "Tipo de servicio", visible: true, type: "foreign", reference: "servicio"},
-                    {name: "fecha_salida", shortname: "Fech. Salida", longname: "Fecha Salida", visible: true, type: "date", required: true, maxlength: 255, pattern: ""}
+                    {name: "obj_servicio", shortname: "Tipo serv.", longname: "Tipo de servicio", visible: false, type: "foreign", reference: "servicio"},
+                    {name: "fecha_salida", shortname: "Fech. Salida", longname: "Fecha Salida", visible: false, type: "date", required: true, maxlength: 255, pattern: ""}
                 ];
             },
             getIcon: function () {
