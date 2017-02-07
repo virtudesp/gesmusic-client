431f5ae08dc997ac3b990af902b2f525a81ec812
raznara <raznara@S07-PROFE>
2017-02-07 17:05:43 +0100

    password change

--- public_html/js/system/passchange.js
+++ public_html/js/system/passchange.js
@@ -3,25 +3,26 @@ moduloSistema.controller('PasschangeController', ['$scope', '$routeParams', '$lo
     function ($scope, $routeParams, $location, serverService, sessionService) {
         $scope.title = "Formulario de cambio de password";
         $scope.icon = "fa-key";
-        $scope.user = {};
-        if (serverService.debugging()) {
-            $scope.old = '';
-            $scope.new = '';
-        }
+        $scope.debugging = serverService.debugging();
+        $scope.status = null;
+        $scope.old = '';
+        $scope.new = '';
         $scope.passchange = function () {
             serverService.getPasswordChangePromise($scope.old, $scope.new).then(function (response) {
+                $scope.response = response;
                 if (response.status == 200) {
                     if (response.data.status == 200) {
-                        $scope.status = null;
+                        $scope.status = "El password se ha cambiado";
                         $scope.result = response.data.message;
+
                     } else {
-                        $scope.status = "Error en la recepción de datos del servidor";
+                        $scope.status = "No se ha cambiado el password";
                     }
                 } else {
-                    $scope.status = "Error en la recepción de datos del servidor";
+                    $scope.status = "No se ha cambiado el password";
                 }
             }).catch(function (data) {
-                $scope.status = "Error en la recepción de datos del servidor";
+                $scope.status = "No se ha cambiado el password";
             });
         }
     }]);
